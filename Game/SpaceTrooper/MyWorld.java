8860f37e17dc8dd27e4228be0614a9f1a160847c
emeliaaja <rizzerbla@gmail.com>
2018-05-11 09:10:06 +0800

    pull lim

--- Game/SpaceTrooper/MyWorld.java
+++ Game/SpaceTrooper/MyWorld.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     public Rocketship rocket=new Rocketship();
+    public Asteroid aster=new Asteroid();
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -18,5 +19,6 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject(rocket,getWidth()/6,getHeight()/2);
+        addObject(aster,);
     }
 }
